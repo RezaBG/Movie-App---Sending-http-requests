4298a94fae467e7a7ed909749f3dab629c7d7bb1
rezaBG <rezabarzegargashti@gmail.com>
2023-06-04 09:59:04 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -12,9 +12,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
-        throw new Error("Something went wrong!");
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
