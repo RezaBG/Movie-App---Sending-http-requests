21e63a70ecbb1073c964433d3c2e556a9e36861a
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-06-04 08:50:30 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+import App from "./App";
 
-//1. Apply CSS styles to App.jsx component
-//to match the appearance on the completed app:
-//https://c6fkx.csb.app/
-//2. Extract the contact card as a reusable Card component.
-//3. Use props to render the default Beyonce contact card.
-//4. Use the contacts.js file to create card components.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <App />
+  </div>
+);
