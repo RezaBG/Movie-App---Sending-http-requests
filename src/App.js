ccb2c9917d40f5f3967299a6a8f0166e22caae2c
rezaBG <rezabarzegargashti@gmail.com>
2023-06-04 09:42:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,15 +8,17 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback ((async) => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
-    setError(null)
+    setError(null);
     try {
       const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error ("Something went wrong!")
+        throw new Error("Something went wrong!");
       }
+
       const data = await response.json();
+
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -24,26 +26,30 @@ function App() {
           openingText: movieData.opening_crawl,
           releaseDate: movieData.release_date,
         };
-    })
-    setMovies(transformedMovies);
-    } catch(error) {
-      setError(error.message)
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
     }
     setIsLoading(false);
-  }, [])
+  }, []);
 
-  useEffect (() => {
-    fetchMoviesHandler()
-  }, [fetchMoviesHandler])
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
-  let content = <p>Found no movies.</p>
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />
+    content = <MoviesList movies={movies} />;
   }
-  
-  if  (error){
-    content = <p>Loading...</p>
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -51,9 +57,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-      {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
