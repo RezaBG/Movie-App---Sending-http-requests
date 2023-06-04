7f4a39d5b9439b0e2595d608faf7b6d8e5517f6e
rezaBG <rezabarzegargashti@gmail.com>
2023-06-04 08:57:58 +0200

    update test

--- src/compnents/Move.js
+++ src/compnents/Move.js
@@ -6,6 +6,7 @@ const Movie = (props) => {
   return (
     <li className={classes.movies}>
       <h2>{props.title}</h2>
+      <p>Hi</p>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
     </li>
